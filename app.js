9dc558062685c8bf8d0258fc21662d61cdaecb73
ganchock <ganchock@yahoo.com>
2022-01-03 18:38:22 +0800

    Orlogo zarlaga delgets deer gargana

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var uiController = (function() {
         inputDescription: ".add__description",
         inputValue: ".add__value",
         addBtn: ".add__btn"
+    };
 
-    }
-    var x = 1245;
     return {
         getInput: function() {
             return {
@@ -16,38 +15,67 @@ var uiController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+
         getDOMstrings: function() {
             return DOMstrings;
+        },
+
+        addListItem: function(item, type) {
+            // Орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ.
+            var html, list;
+            if (type === 'inc') {
+                list = ".income__list";
+                html =
+                    '<div class="item clearfix" id="income-%id%"><div class = "item__description">$$DESCRIPTION$$</div><div class = "right clearfix" ><div class = "item__value" > $$VALUE$$ </div><div class = "item__delete" ><button class = "item__delete--btn" > < i class = "ion-ios-close-outline" > < /i></button ></div> </div></div>';
+            } else {
+                list = ".expenses__list";
+                html = '<div class="item clearfix" id="expense-%id%"></div><div class="item__description">$$DESCRIPTION$$</div><div class="right clearfix"><div class="item__value"> $$VALUE$$ </div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Тэр HTML дотроо орлого зарлагын утгуудыг REPLACE ашиглан өөрчилж өгнө.
+            html = html.replace('%id%', item.id);
+            html = html.replace('$$DESCRIPTION$$', item.description);
+            html = html.replace('$$VALUE$$', item.value);
+
+
+            // Бэлтгэсэн HTML ээ DOM руу хийж өгнө.
+            document.querySelector(list).insertAdjacentHTML("beforeend", html);
         }
     };
 })();
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {
+    // private data
     var Income = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
+
+    // private data
     var Expense = function(id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
+    // private data
     var data = {
         items: {
             inc: [],
             exp: []
         },
+
         totals: {
             inc: 0,
             exp: 0
         }
     };
+
     return {
-        addItems: function(type, desc, val) {
-            var items, id;
+        addItem: function(type, desc, val) {
+            var item, id;
             if (data.items[type].length === 0) id = 1;
             else {
                 id = data.items[type][data.items[type].length - 1].id + 1;
@@ -58,6 +86,8 @@ var financeController = (function() {
                 item = new Expense(id, desc, val);
             }
             data.items[type].push(item);
+
+            return item;
         },
         seeData: function() {
             return data;
@@ -72,10 +102,10 @@ var appController = (function(uiController, fnController) {
     var ctrlAddItem = function() {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
         var input = uiController.getInput();
-        console.log(input)
-            // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд холбоно.
-        financeController.addItems(input.type, input.description, input.value);
+        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд холбоно.
+        var item = financeController.addItem(input.type, input.description, input.value);
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+        uiController.addListItem(item, input.type);
         // 4. Төсвийн тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     };
@@ -94,7 +124,7 @@ var appController = (function(uiController, fnController) {
     };
     return {
         init: function() {
-            console.log('Application started ...');
+            // console.log('Application started ...');
             setUpEventListeners();
         }
     }
