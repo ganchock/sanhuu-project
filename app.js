8567403d7ff464ccbd27a9b7755cf10b3ba43865
ganchock <ganchock@yahoo.com>
2022-01-03 10:23:54 +0800

    EventListener is created

---
+++ app.js
@@ -0,0 +1,26 @@
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function() {})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function() {})();
+
+// Програмын холбогч контроллер
+var appController = (function(uiController, fnController) {
+    var ctrlAddItem = function() {
+        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+        console.log("Дэлгэцээс өгөгдлөө авах хэсэг")
+            // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд холбоно.
+            // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+            // 4. Төсвийн тооцоолно.
+            // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    };
+
+    document.querySelector(".add__btn").addEventListener("click", function() {
+        ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) { ctrlAddItem(); }
+
+    });
+})(uiController, financeController);
